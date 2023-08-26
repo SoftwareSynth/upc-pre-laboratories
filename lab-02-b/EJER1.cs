f7648608622cffed1c6d3a97302facae5ec46ba8
Ryan Sweden <89040278+RyanSweden03@users.noreply.github.com>
2023-08-25 23:16:06 -0500

    Fix : Corrected Ejer1

---
+++ lab-02-b/EJER1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> listOfUsers = new List<User>()
+        {
+        new User() { Name = "John Doe", Age = 42 },
+        new User() { Name = "Jane Doe", Age = 34 },
+        new User() { Name = "Joe Doe", Age = 8 },
+        };
+
+            foreach (User user in listOfUsers)
+            {
+                Console.WriteLine($"{user.Name} is {user.Age} years old");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
